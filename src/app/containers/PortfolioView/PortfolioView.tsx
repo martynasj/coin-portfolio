6071b97fbc16287ad9709534bd08ae800deef5f2
martynasj <lyrinisobjektas@gmail.com>
2018-01-07 14:46:48 +0000

    Add coin ui

--- src/app/containers/PortfolioView/PortfolioView.tsx
+++ src/app/containers/PortfolioView/PortfolioView.tsx
@@ -22,6 +22,28 @@ export class PortfolioView extends React.Component<Props> {
     this.props.portfolio.syncPortfolio(this.props.match.params.id)
   }
 
+  private handleAddNewCoin = () => {
+    const input = window.prompt(
+      `Type in the following separated by comas: coin symbol, number of units, buy price
+
+       Example:
+       xrp, 1200, 2.64
+      `
+    )
+    if (input) {
+      const tokens = input.toLowerCase().split(',').map(token => token.trim())
+      if (tokens.length !== 3) {
+        alert('Wrong input you dumb fuck')
+        return
+      }
+      const symbolId = tokens[0]
+      const numberOfUnits = parseFloat(tokens[1])
+      const buyPrice = parseFloat(tokens[2])
+
+      console.log(symbolId, numberOfUnits, buyPrice)
+    }
+  }
+
   renderLoading = () => {
     return (
       <div>Loading</div>
@@ -58,23 +80,22 @@ export class PortfolioView extends React.Component<Props> {
         />
         {portfolio.items.map(item => {
           return (
-          <div key={item.id}>
-            <PortfolioItem
-              key={item.id}
-              symbol={item.symbolId}
-              buyPrice={item.pricePerUnitPaid}
-              currentPrice={item.currentPrice}
-              numberOfUnits={item.numberOfUnits}
-              change={item.change}
-              changePercentage={item.changePercentage}
-            />
-            <button onClick={() => {
-              item.setNumberOfUnits(item.numberOfUnits + 1)
-            }}>
-              Add 1 unit
-            </button>
-          </div>)
+            <div key={item.id}>
+              <PortfolioItem
+                key={item.id}
+                symbol={item.symbolId}
+                buyPrice={item.pricePerUnitPaid}
+                currentPrice={item.currentPrice}
+                numberOfUnits={item.numberOfUnits}
+                change={item.change}
+                changePercentage={item.changePercentage}
+              />
+            </div>
+          )
         })}
+        <button onClick={this.handleAddNewCoin}>
+          Add A Coin
+        </button>
       </div>
     )
   }
