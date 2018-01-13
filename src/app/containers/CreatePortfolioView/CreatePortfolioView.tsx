4787f4fcec3632d23464ffe4e19a63f9b46d5427
ViliusLinge <viliusling@gmail.com>
2018-01-13 23:02:37 +0200

    front-page-styling-2

--- src/app/containers/CreatePortfolioView/CreatePortfolioView.tsx
+++ src/app/containers/CreatePortfolioView/CreatePortfolioView.tsx
@@ -28,66 +28,75 @@ export default class CreatePortfolioView extends React.Component<Props> {
 
   render() {
     return (
-      <div>
+      <div style={{background: 'linear-gradient(-20deg, #090e58, #6ed8e8)'}}>
 
-        <div style={{
-          minHeight: '100vh', 
-          display: 'table', 
-          position: 'relative', 
-          marginLeft: 'auto', 
-          marginRight: 'auto', 
-          textAlign: 'center'
-           }}>
+        <div 
+          style={{
+            minHeight: '100vh', 
+            display: 'flex', 
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center',
+          }}
+        >
 
-          <div style={{
-            width: '55vw', 
-            color: 'white', 
-            display: 'table-cell', 
-            verticalAlign: 'middle'
-             }}>
+          <div
+            style={{
+              width: '55vw', 
+              color: 'white', 
+            }}
+          >
 
-            <div style={{
-              fontSize: '7rem',
-              margin: 0,
-              fontWeight: 300,
-               }}>
+            <div
+              style={{
+                fontSize: '7rem',
+                margin: 0,
+                fontWeight: 300,
+              }}
+            >
               <span style={{color: 'rgb(13, 22, 42)'}}>Shit</span>
               <span>fol.io</span>
             </div>
 
-            <h2 style={{
-              maxWidth: '70%', 
-              fontSize: '2.2rem', 
-              textAlign: 'left',
-              textTransform: 'capitalize' 
-               }}>
+            <h2
+              style={{
+                maxWidth: '70%', 
+                fontSize: '2.2rem', 
+                textAlign: 'left',
+                textTransform: 'capitalize' 
+              }}
+            >
               The ultimate cryptocurrency portfolio tracker tool for your desktop.
             </h2>
 
-            <p style={{
-              maxWidth: '70%', 
-              marginLeft: 'auto',
-              fontSize: '1.4rem',
-              textAlign: 'right',
-              color: 'rgb(13, 22, 42)'
-               }}> 
+            <p
+              style={{
+                maxWidth: '70%', 
+                marginLeft: 'auto',
+                fontSize: '1.4rem',
+                textAlign: 'right',
+                color: 'rgb(13, 22, 42)'
+              }}
+            > 
               Manage all your cryptocurrencies, including Bitcoin, Ethereum, Litecoin and over 2000 alt coins.
             </p>
 
             <div>
-              <button style={{
-                border: 'none',
-                minWidth: '240px',
-                borderRadius: '5px',
-                backgroundColor: '#0e162b',
-                color: '#b1c7cc',
-                fontSize: '1rem',
-                fontWeight: 500,
-                lineHeight: 3,
-                margin: '20px',
-                cursor: 'pointer'
+              <button 
+                style={{
+                  border: 'none',
+                  minWidth: '240px',
+                  borderRadius: '5px',
+                  backgroundColor: '#0e162b',
+                  color: '#b1c7cc',
+                  fontSize: '1rem',
+                  fontWeight: 500,
+                  lineHeight: 3,
+                  margin: '20px',
+                  cursor: 'pointer'
                 }}
-                onClick={this.handleCreateNewPortfolio}>
+                onClick={this.handleCreateNewPortfolio}
+              >
                 Create New Portfolio
                </button>
             </div>
@@ -96,20 +105,24 @@ export default class CreatePortfolioView extends React.Component<Props> {
 
         </div>
 
-        <div style={{
-          position: 'fixed',
-           top: 'calc(100vh - 60px)',
-           width: '100vw'
-          }}>
+        <div
+          style={{
+            position: 'fixed',
+            top: 'calc(100vh - 60px)',
+            width: '100vw'
+          }}
+        >
 
-          <div style={{
-            width: '90%',
-            display: 'flex',
-            justifyContent: 'space-between',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            color: '#618e97'
-            }}>
+          <div
+            style={{
+              width: '90%',
+              display: 'flex',
+              justifyContent: 'space-between',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              color: '#618e97'
+            }}
+          >
 
             <span>Binance</span>
             <span>Bitfinex</span>
