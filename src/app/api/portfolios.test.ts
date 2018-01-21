a48ef4aa9bd08b4f3939ec071759b8d0ceb9653f
martynasj <lyrinisobjektas@gmail.com>
2018-01-21 14:09:06 +0000

    Fix test setup

--- src/app/api/portfolios.test.ts
+++ src/app/api/portfolios.test.ts
@@ -1,7 +1,12 @@
+import { initFirebase } from '../../../setup/test-utils'
 import * as portfolioService from './portfolios'
 
 describe('api/portfolios', () => {
-  test('abc', async () => {
+  beforeAll(() => {
+    initFirebase()
+  })
+
+  test('create new portfolio', async () => {
     await portfolioService.createNewPortfolio('one-two')
     const portfolio = await portfolioService.fetchPortfolio('one-two')
     expect(portfolio.id).toBe('one-two')
